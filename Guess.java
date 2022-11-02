2344e6126f5f1a922a040a902ff0d5bfddb747bd
Nisha Jha <83752180+Nisha213-mar@users.noreply.github.com>
2022-11-02 17:38:28 +0530

    Project2

---
+++ Guess.java
@@ -0,0 +1,53 @@
+
+
+import java.util.*;
+
+public class Guess {
+
+    public static void guessingNumberGame() {
+        Scanner in = new Scanner(System.in);
+        int number = 1 + (int) (100 * Math.random());
+        int k = 3;
+        int guess, i;
+        System.out.println("A number is chosen"
+                + " between 1 to 100."
+                + "Guess the number"
+                + " within 3 trials.");
+        for (i = 0; i < k; i++) {
+            System.out.println("Guess the Number ");
+            guess = in.nextInt();
+            
+            if( guess == number) {
+                        System.out.println("Congratulations!" + " You guessed the number.");
+                    break;
+            }
+                   else if (number > guess && i != k - 1){
+                        System.out.println(
+                                "The number is "+ "greater than " + guess+"guess the lesser one");
+                 
+                    }
+            
+                    else if (number < guess && i != k - 1){
+                        System.out.println(
+                                "The number is"+ " less than " + guess+"guess the larger one");
+                    
+                    }
+                }
+
+            
+
+      
+        if (i == k) {
+            System.out.println(
+                "You have exhausted"
+                + " K trials.");
+ 
+            System.out.println(
+                "The number was " + number);
+        }
+    }
+
+    public static void main(String[] args) {
+        guessingNumberGame();
+    }
+}
